dbde0d3393dd7bf9d97dfdbf1651e40812703dd0
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-08-30 11:09:20 -0300

    Classes defined

---
+++ CompositionPosts/Entities/Comment.cs
@@ -0,0 +1,13 @@
+﻿namespace CompositionPosts.Entities {
+    public class Comment {
+        // Attributes
+        public string Text { get; set; }
+
+        // Constructors
+        public Comment() { }
+
+        public Comment(string text) {
+            Text = text;
+        }
+    }
+}
---
+++ CompositionPosts/Entities/Post.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositionPosts.Entities {
+    public class Post {
+        // Attributes
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Conten { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        // Constructors
+        public Post() {}
+
+        public Post(DateTime moment, string title, string conten, int likes) {
+            Moment = moment;
+            Title = title;
+            Conten = conten;
+            Likes = likes;
+        }
+
+        // Methods
+        public void AddComment(Comment comment) { 
+            Comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment) {
+            Comments.Remove(comment);
+        }
+    }
+}
