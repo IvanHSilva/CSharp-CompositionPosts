37d134eebf141f87ac8160eda5172b95c3252d4d
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-08-30 11:24:21 -0300

    Program print posts and comments

--- CompositionPosts/Entities/Post.cs
+++ CompositionPosts/Entities/Post.cs
@@ -1,35 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace CompositionPosts.Entities {
     public class Post {
         // Attributes
         public DateTime Moment { get; set; }
         public string Title { get; set; }
-        public string Conten { get; set; }
+        public string Content { get; set; }
         public int Likes { get; set; }
         public List<Comment> Comments { get; set; } = new List<Comment>();
 
         // Constructors
-        public Post() {}
+        public Post() { }
 
-        public Post(DateTime moment, string title, string conten, int likes) {
+        public Post(DateTime moment, string title, string content, int likes) {
             Moment = moment;
             Title = title;
-            Conten = conten;
+            Content = content;
             Likes = likes;
         }
 
         // Methods
-        public void AddComment(Comment comment) { 
+        public void AddComment(Comment comment) {
             Comments.Add(comment);
         }
 
         public void RemoveComment(Comment comment) {
             Comments.Remove(comment);
+        }
+
+        // Override
+        public override string ToString() {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Curtidas - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comentários:");
+            foreach (Comment com in Comments) {
+                sb.AppendLine(com.Text);
+            }
+            return sb.ToString();
         }
     }
 }
--- CompositionPosts/Program.cs
+++ CompositionPosts/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CompositionPosts.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,23 @@ using System.Threading.Tasks;
 namespace CompositionPosts {
     internal class Program {
         static void Main(string[] args) {
+
+            // Create posts and commentaries
+            Comment c1 = new Comment("Tenha uma boa viagem!");
+            Comment c2 = new Comment("Isto é muito legal, cara!");
+            Post p1 = new Post(DateTime.Parse("21/06/2018 13:05:44"), "Viagem à Nova Zelândia", "Estou visitando um país novo e muito interessante!", 12);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Boa noite!");
+            Comment c4 = new Comment("Que a força esteja com você");
+            Post p2 = new Post(DateTime.Parse("28/07/2018 23:14:20"), "Boa noite pessoal", "Vejo vocês amanhã", 5);
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            // Print the posts
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
         }
     }
 }
